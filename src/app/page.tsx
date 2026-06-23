848ffe5944afbb8ee7998e13d3866d946cb02740
pedrobarrosgg1-a11y <pedro.barros.gg1@gmail.com>
2026-06-23 20:00:44 -0300

    Setting up ESLint, Prettier, and Lefthook.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-   <div>
-    <h2 className="text-gray-100 text-center mt-32">Hello World!</h2>
-   </div>
+    <div>
+      <h2 className="text-gray-100 text-center mt-32">Hello World!</h2>
+    </div>
   );
 }
